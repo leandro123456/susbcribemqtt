2a227f2da44bb87a6a8122230d75ccbc9003432e
Leandro Guzman <leandro.guzman@movasim.com>
2019-11-04 16:47:45 -0300

    envio comandos push

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,20 +6,20 @@ import Persistence.DAO.DeviceDAO;
 import Persistence.Model.Device;
 import Persistence.Model.DeviceConfiguration;
 
-import java.util.UUID;
-
-import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloController {
 	private DeviceDAO devdao =  new DeviceDAO();
 	DeviceConfiguration devconf = null;
     
+	
+	//ENVIO DE MENSAJES DE ALARMA
     @RequestMapping("envio/{serial}/{valor}")
     public String index(@PathVariable String valor, @PathVariable String serial) {
     	try {
@@ -68,5 +68,35 @@ public class HelloController {
 		}
         return "envio_exitoso";
     }
+    
+    @RequestMapping(value="enviopulsador/{serial}", method=RequestMethod.POST, produces="application/json")
+	public String SendMessagePulsador(@PathVariable String serial,@RequestBody String json) {
+    	try {
+    		Device device = devdao.retrieveBySerialNumber(serial);
+    		if(device.getUsedefaultbrocker())
+    			devconf= device.getDeviceconfiguration().get(0);
+    		else
+    			devconf= device.getDeviceconfiguration().get(1);
+    		
+    		MqttClient cliente = MqttConnect.getInstance().getClient();
+    		System.out.println("*************************ESTA CONECTADA???????? "+cliente.isConnected());
+    		if(!cliente.isConnected()){
+    			MqttConnectOptions options = new MqttConnectOptions();
+    			options.setAutomaticReconnect(true);
+    			options.setUserName("cDashSVR");
+    			options.setPassword("av1vEDacfGwXc5".toCharArray());
+    			cliente.connect(options);
+    		}
+    		EnviodeMensajes env = new EnviodeMensajes(cliente, json,devconf.getTopicescribir());
+			env.call();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fallo_envio";
+		}
+        return "envio_exitoso";
+    }
+   
+    
+    
     
 }
